8894488d43bccfc3fe7231b3e7d802b98744c4dc
RBA-Infotech <109081076+RBA-Infotech@users.noreply.github.com>
2022-07-15 15:02:29 +0530

    Add files via upload

--- Java8Features/StreamDemo.java
+++ Java8Features/StreamDemo.java
@@ -10,67 +10,54 @@ public class StreamDemo {
 	public static void main(String[] args) {
 
 		/************************************
-		 * Intermediate Operation 
-		 * filter() 
-		 * map() 
-		 * sorted() 
+		 * 1. Intermediate Operation filter() map() sorted()
 		 * 
 		 * 
 		 ************************************/
 
 		/************************************
-		 * Aggregate or Terminal Operation 
-		 * forEach() 
-		 * reduce() 
-		 * collect() 
-		 * count() 
-		 * min() 
-		 * max()
-		 * findFirst()
-		 * anyMatch()
-		 * allMatch()
+		 * 2. Aggregate or Terminal Operation forEach() reduce() collect() count() min()
+		 * max() findFirst() anyMatch() allMatch()
 		 ************************************/
 
 		List<Integer> list = Arrays.asList(1, 8, 3, 4, 2, 5, 6);
 
 		// below code on how to use filter
 //		list.stream()
-//			.filter(x -> x % 2 == 0)
+//			.filter(x -> x % 2 == 0)  // (8, 4, 2, 6)
 //			.forEach(x -> System.out.println(x));
-//			//forEach(System.out::println);
+		// forEach(System.out::println);
 
 		// below code on how to use map
 //		list.stream()
 //		.map(x -> x * x)
-//		.forEach(System.out::println);
+//		.forEach(x -> System.out.println(x));
+		// .forEach(System.out::println);
 
 		// below code on how to use sort
 //		list.stream()
 //			.sorted()
-//			.forEach(System.out::println);
+//			.forEach(x -> System.out.println(x));
+		// .forEach(System.out::println);
 
 		// below code on how to use reduce
-		int total = list.stream()
-						.reduce(0, (e1, e2) -> e1 + e2); // (0, 9), (9, 3), (12, 4), (16, 2)
-
-		System.out.println(total);
+//		int total = list.stream()  // (1, 8, 3, 4, 2, 5, 6)
+//						.reduce(0, (e1, e2) -> e1 + e2); // (0, 9),(9,3),(12, 4),(16, 2),(18, 5),(23, 6) 
+//
+//		System.out.println(total);
 
 //		long cnt = list.stream()
 //						.count();
 //
 //		System.out.println(cnt);
+
+		List<Integer> oddNo = list.stream()
+				.filter(x -> x % 2 != 0)
+				.collect(Collectors.toList());
 		
-//		List<Integer> oddNo = list.stream()
-//									.filter(x -> x % 2 != 0)
-//									.collect(Collectors.toList());
-//									
-//		list.stream()
-//			.filter(x -> x % 2 != 0)
-//			.forEach(x -> System.out.println(x));
-//										
-//		oddNo.forEach(x -> System.out.println(x));
-		
-		
+		oddNo.forEach(x -> System.out.println(x));
+
+
 	}
 
 }
